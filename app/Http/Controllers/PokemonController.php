5bcdfe312a8ac402cf02f954667c9cbea3e61ffd
Jean-Marc <jeanmarc.yekoumbet@gmail.com>
2026-02-18 23:01:28 +0100

    homepokemons

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -8,6 +8,6 @@ class PokemonController extends Controller
 {
     //Fonction qui retournera notre vue pokemon(liste de tous les pokemons)
     public function index(){
-        return view('homepokemon');
+        return view('homepokemons');
     }
 }
