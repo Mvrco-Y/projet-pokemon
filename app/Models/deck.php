8785e0f8f69d55fca5a438b702ce08d5103c5af9
Jean-Marc <jeanmarc.yekoumbet@gmail.com>
2026-02-24 15:39:07 +0100

    Creation de nos relations eloquent entre nos deux tables et deck & pokemon ainsi que des test tinker

--- app/Models/deck.php
+++ app/Models/deck.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Pokemon;
 
-class deck extends Model
+class Deck extends Model
 {
     protected $fillable = ['name', 'description', 'user_id'];
+    
+    public function pokemons()
+        {
+            return $this->belongsToMany(Pokemon::class, 'deck_pokemon')
+                ->withPivot('quantity')
+                ->withTimestamps();
+        }
+
 }
--- app/Models/pokemon.php
+++ app/Models/pokemon.php
@@ -3,11 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Deck;
 
 class Pokemon extends Model
 {
     
     protected $table = 'pokemon';
     public $timestamps = false;
+    
+    public function decks()
+    {
+        return $this->belongsToMany(Deck::class, 'deck_pokemon')
+            ->withPivot('quantity')
+            ->withTimestamps();
+    }
+
 
 }
